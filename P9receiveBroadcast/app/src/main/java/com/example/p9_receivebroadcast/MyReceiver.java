fc0ba1f4632a3b460979f316790adc0cb7572963
Haodong Lu <lhd32262175@gmail.com>
2020-05-24 13:50:44 +1000

    receiver

---
+++ P9receiveBroadcast/app/src/main/java/com/example/p9_receivebroadcast/MyReceiver.java
@@ -0,0 +1,27 @@
+package com.example.p9_receivebroadcast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+//public class MyReceiver extends BroadcastReceiver {
+//
+//    @Override
+//    public void onReceive(Context context, Intent intent) {
+//        Toast.makeText(context, "Broadcast recieved !", Toast.LENGTH_LONG).show();
+//    }
+//}
+public class MyReceiver extends BroadcastReceiver {
+    private static final String TAG = "MyBroadcastReceiver";
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Action: " + intent.getAction() + "\n");
+        sb.append("URI: " + intent.toUri(Intent.URI_INTENT_SCHEME).toString() + "\n");
+        String log = sb.toString();
+        Log.d(TAG, log);
+        Toast.makeText(context, log, Toast.LENGTH_LONG).show();
+    }
+}
